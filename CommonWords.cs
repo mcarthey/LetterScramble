b2d72fff9437da7a3f1eddd835dc97a81e379adb
Mark McArthey <mcarthey@gmail.com>
2022-02-04 08:53:53 -0600

    Add project files.

---
+++ CommonWords.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LetterScramble
+{
+    public static class CommonWords
+    {
+        public static string[] ThreeLetterWords { get; } = new[]
+        {
+            "the", "and", "are", "for", "not", "but", "had", "has", "was", "all", "any", "one", "man", "out", "you", "his", "her", "can"
+        };
+
+        public static string[] FourLetterWords { get; } = new[]
+        {
+            "that", "with", "have", "this", "will", "your", "from", "they", "want", "been", "good", "much", "some", "very"
+        };
+
+        public static Dictionary<int, List<string>> PointValues { get; } = new Dictionary<int, List<string>>()
+        {
+            {1, new List<string>() {"A", "E", "I", "O", "U", "L", "N", "S", "T", "R"}},
+            {2, new List<string>() {"D", "G"}},
+            {3, new List<string>() {"B", "C", "M", "P"}},
+            {4, new List<string>() {"F", "H", "V", "W", "Y"}},
+            {5, new List<string>() {"K"}},
+            {8, new List<string>() {"J", "X"}},
+            {10, new List<string>() {"Q", "Z"}},
+        };
+
+        public static Dictionary<string, int> LetterValues { get; } = new Dictionary<string, int>()
+        {
+            { "A", 1 },
+            { "B", 3 },
+            { "C", 3 },
+            { "D", 2 },
+            { "E", 1 },
+            { "F", 4 },
+            { "G", 2 },
+            { "H", 4 },
+            { "I", 1 },
+            { "J", 8 },
+            { "K", 5 },
+            { "L", 1 },
+            { "M", 3 },
+            { "N", 1 },
+            { "O", 1 },
+            { "P", 3 },
+            { "Q", 10 },
+            { "R", 1 },
+            { "S", 1 },
+            { "T", 1 },
+            { "U", 1 },
+            { "V", 4 },
+            { "W", 4 },
+            { "X", 8 },
+            { "Y", 4 },
+            { "Z", 10 }
+        };
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Spectre.Console;
+
+namespace LetterScramble
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var words = GenerateWords();
+            var scrambled = ScrambleWords(words);
+
+            OutputTable(scrambled);
+
+            Console.WriteLine("Enter word:");
+            var word = Console.ReadLine();
+
+            var wordValue = CalculateWordValue(word);
+
+            Console.WriteLine($"Your word value is {wordValue}");
+        }
+
+        private static int CalculateWordValue(string? word)
+        {
+            int pointValue = 0;
+
+            if (word == null)
+            {
+                return pointValue;
+            }
+
+            foreach (char c in word)
+            {
+                string letter = c.ToString().ToUpper();
+                pointValue += CommonWords.LetterValues[letter];
+            }
+
+            return pointValue;
+        }
+
+        private static void OutputTable(List<string> scrambled)
+        {
+            // Create a table
+            var table = new Table();
+            table.HideHeaders();
+
+            // Add some columns
+            table.AddColumn("");
+            table.AddColumn("");
+            table.AddColumn("");
+            table.AddColumn("");
+
+            for (int i = 0; i < scrambled.Count; i++)
+            {
+                table.AddRow(scrambled[i], scrambled[i+1], scrambled[i+2], scrambled[i+3]);
+                i += 3;
+            }
+
+            // Render the table to the console
+            AnsiConsole.Write(table);
+        }
+
+        private static List<string> ScrambleWords(List<string> words)
+        {
+            string combinedString = string.Join("", words.ToArray());
+            List<string> scrambledLetters = new List<string>();
+
+            Random r = new Random();
+            foreach (int i in Enumerable.Range(0, 16).OrderBy(x => r.Next()))
+            {
+                scrambledLetters.Add(combinedString.Substring(i, 1));
+            }
+
+            return scrambledLetters;
+        }
+
+        public static List<string> GenerateWords()
+        {
+            List<string> words = new List<string>();
+
+            var longWord = "suspicious";
+            var vowels = "aeiou";
+
+            var mod3 = (16 - longWord.Length) % 3;
+            var mod4 = (16 - longWord.Length) % 4;
+
+
+            var random = new Random();
+            if (mod3 == 0)
+            {
+                for (int j = 0; j < (16 - longWord.Length) / 3; j++)
+                {
+                    var i = random.Next(0, CommonWords.ThreeLetterWords.Length - 1);
+                    words.Add(CommonWords.ThreeLetterWords[i]);
+                }
+            }
+            if (mod4 == 0)
+            {
+                for (int j = 0; j < (16 - longWord.Length) / 3; j++)
+                {
+                    var i = random.Next(0, CommonWords.FourLetterWords.Length - 1);
+                    words.Add(CommonWords.FourLetterWords[i]);
+                }
+            }
+
+            words.Add(longWord);
+
+            words.ForEach(Console.WriteLine);
+            return words;
+
+        }
+    }
+}
